35a22fe304d5281d2755982fd91ecef0d750cda1
Aryan Kumar <aryankumar20092004@gmail.com>
2026-02-19 22:17:58 +0530

    Add insertion sort implementation in C
    Implement insertion sort algorithm and test it in main function.

--- Sorting/insertion_sort.c
+++ Sorting/insertion_sort.c
@@ -1,1 +1,29 @@
+#include <stdio.h>
 
+void insertionSort(int arr[], int n) {
+    int i, key, j;
+    for (i = 1; i < n; i++) {
+        key = arr[i];
+        j = i - 1;
+
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+int main() {
+    int arr[] = {8, 21, 14, 9, };
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int i;
+
+    insertionSort(arr, n);
+
+    printf("Sorted array: ");
+    for (i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+
+    return 0;
+}
